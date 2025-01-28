a22b9a7d1d0681bec812d84de30b5eec8f8c43e5
Riko <riko.fluchel@gmail.com>
2025-01-28 07:56:21 -0500

    complete lab

--- main.js
+++ main.js
@@ -3,8 +3,42 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+//get all heart glyphs in a node list
+const hearts = document.querySelectorAll("span.like-glyph");
 
+hearts.forEach((heart) => {
+	heart.addEventListener("click", function() {
+		mimicServerCall()
+		.then((res) => {
+			//if the server response is successful,
+			//check if the heart is empty
+			if (heart.textContent === EMPTY_HEART){
+				//set the heart textContent to a full heart
+				heart.textContent = FULL_HEART;
+				//add activated heart class to the heart
+				heart.classList.add("activated-heart")
+			}
+			//check is the heart is already full
+			else if (heart.textContent === FULL_HEART) {
+				//set the heart textContent to an empty heart
+				heart.textContent = EMPTY_HEART;
+				//remove the activated heart from the class list
+				heart.classList.remove("activated-heart")
+			}
 
+		})
+		.catch((error) => {
+			//if there's a server error, remove the hidden class from error modal
+			const errorModal = document.getElementById("modal");
+			errorModal.classList.remove("hidden")
+
+			//after 3 seconds, add the hidden class back to error modal
+			setTimeout(function(){
+				errorModal.classList.add("hidden")
+			}, 3000)
+		})
+	});
+});
 
 
 //------------------------------------------------------------------------------
